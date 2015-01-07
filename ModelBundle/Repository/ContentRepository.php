39eb62b3145860682bb6aa7bda183e96bec99501
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-01-07 10:25:10 +0100

    duplicate content

--- ModelBundle/Repository/ContentRepository.php
+++ ModelBundle/Repository/ContentRepository.php
@@ -3,6 +3,7 @@
 namespace PHPOrchestra\ModelBundle\Repository;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
+use PHPOrchestra\BaseBundle\Context\CurrentSiteIdInterface;
 use PHPOrchestra\ModelBundle\Repository\FieldAutoGenerableRepositoryInterface;
 use PHPOrchestra\ModelInterface\Model\ContentInterface;
 use PHPOrchestra\ModelInterface\Repository\ContentRepositoryInterface;
@@ -12,6 +13,19 @@ use PHPOrchestra\ModelInterface\Repository\ContentRepositoryInterface;
  */
 class ContentRepository extends DocumentRepository implements FieldAutoGenerableRepositoryInterface, ContentRepositoryInterface
 {
+    /**
+     * @var CurrentSiteIdInterface
+     */
+    protected $currentSiteManager;
+
+    /**
+     * @param CurrentSiteIdInterface $currentSiteManager
+     */
+    public function setCurrentSiteManager(CurrentSiteIdInterface $currentSiteManager)
+    {
+        $this->currentSiteManager = $currentSiteManager;
+    }
+
     /**
      * Get all content if the contentType is "news"
      *
@@ -72,10 +86,64 @@ class ContentRepository extends DocumentRepository implements FieldAutoGenerable
      * @param string $contentId
      * @param string $language
      *
-     * @return ContentInterface
+     * @return mixed
      */
     public function findOneByContentIdAndLanguage($contentId, $language)
     {
-        return $this->findOneBy(array('contentId' => $contentId, 'language' => $language));
+        return $this->findOneByContentIdAndLanguageAndVersion($contentId, $language, null);
+    }
+
+    /**
+     * @param string      $contentId
+     * @param string|null $language
+     *
+     * @return mixed
+     */
+    public function findByContentIdAndLanguage($contentId, $language = null)
+    {
+        return $this->findByContentIdAndLanguageAndVersion($contentId, $language, false);
+    }
+
+    /**
+     * @param string      $contentId
+     * @param string|null $language
+     * @param int|null    $version
+     *
+     * @return mixed
+     */
+    public function findOneByContentIdAndLanguageAndVersion($contentId, $language = null, $version = null)
+    {
+        return $this->findByContentIdAndLanguageAndVersion($contentId, $language, $version);
+    }
+
+    /**
+     * @param string      $contentId
+     * @param string|null $language
+     * @param int|null    $version
+     *
+     * @return mixed
+     */
+    public function findByContentIdAndLanguageAndVersion($contentId, $language = null, $version = false)
+    {
+        if (is_null($language)) {
+            $language = $this->currentSiteManager->getCurrentSiteDefaultLanguage();
+        }
+        $qb = $this->createQueryBuilder('c');
+        $qb->field('contentId')->equals($contentId);
+        $qb->field('language')->equals($language);
+        $qb->field('deleted')->equals(false);
+
+        if ($version === false) {
+            $qb->sort('version', 'desc');
+            return $qb->getQuery()->execute();
+        }
+
+        if (is_null($version)) {
+            $qb->sort('version', 'desc');
+            return $qb->getQuery()->getSingleResult();
+        }
+
+        $qb->field('version')->equals((int) $version);
+        return $qb->getQuery()->getSingleResult();
     }
 }
