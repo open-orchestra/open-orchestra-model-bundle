b838d6dc7fea35511231afbe53f07ae9acc37f90
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-01-19 12:20:55 +0100

    add meta

--- ModelBundle/Document/Site.php
+++ ModelBundle/Document/Site.php
@@ -53,6 +53,34 @@ class Site implements SiteInterface
     protected $defaultLanguage;
 
     /**
+     * @var string $metaKeywords
+     *
+     * @MongoDB\Field(type="string")
+     */
+    protected $metaKeywords;
+
+    /**
+     * @var string $metaDescription
+     *
+     * @MongoDB\Field(type="string")
+     */
+    protected $metaDescription;
+
+    /**
+     * @var boolean metaIndex
+     *
+     * @MongoDB\Field(type="boolean")
+     */
+    protected $metaIndex = false;
+
+    /**
+     * @var boolean metaFollow
+     *
+     * @MongoDB\Field(type="boolean")
+     */
+    protected $metaFollow = false;
+
+    /**
      * @var array $languages
      *
      * @MongoDB\Field(type="collection")
@@ -134,6 +162,70 @@ class Site implements SiteInterface
     public function getDefaultLanguage()
     {
         return $this->defaultLanguage;
+    }
+
+    /**
+     * @param string $metaKeywords
+     */
+    public function setMetaKeywords($metaKeywords)
+    {
+        $this->metaKeywords = $metaKeywords;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMetaKeywords()
+    {
+        return $this->metaKeywords;
+    }
+
+    /**
+     * @param string $metaDescription
+     */
+    public function setMetaDescription($metaDescription)
+    {
+        $this->metaDescription = $metaDescription;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMetaDescription()
+    {
+        return $this->metaDescription;
+    }
+
+    /**
+     * @param boolean $index
+     */
+    public function setMetaIndex($metaIndex)
+    {
+        $this->metaIndex = $metaIndex;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getMetaIndex()
+    {
+        return $this->metaIndex;
+    }
+
+    /**
+     * @param boolean $follow
+     */
+    public function setMetaFollow($metaFollow)
+    {
+        $this->metaFollow = $metaFollow;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getMetaFollow()
+    {
+        return $this->metaFollow;
     }
 
     /**
