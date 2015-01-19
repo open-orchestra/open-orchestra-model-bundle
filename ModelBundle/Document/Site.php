d50e94ada7d6801b5629386cc7def0c551e38fd1
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-01-19 15:45:21 +0100

    add meta

--- ModelBundle/Document/Site.php
+++ ModelBundle/Document/Site.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use PHPOrchestra\ModelInterface\Model\SiteInterface;
 use PHPOrchestra\ModelInterface\Model\ThemeInterface;
+use PHPOrchestra\ModelInterface\MongoTrait\MetaableDocument;
 
 /**
  * Description of Site
@@ -17,6 +18,8 @@ use PHPOrchestra\ModelInterface\Model\ThemeInterface;
  */
 class Site implements SiteInterface
 {
+    use MetaableDocument;
+
     /**
      * @var string $id
      *
@@ -51,34 +54,6 @@ class Site implements SiteInterface
      * @MongoDB\Field(type="string")
      */
     protected $defaultLanguage;
-
-    /**
-     * @var string $metaKeywords
-     *
-     * @MongoDB\Field(type="string")
-     */
-    protected $metaKeywords;
-
-    /**
-     * @var string $metaDescription
-     *
-     * @MongoDB\Field(type="string")
-     */
-    protected $metaDescription;
-
-    /**
-     * @var boolean metaIndex
-     *
-     * @MongoDB\Field(type="boolean")
-     */
-    protected $metaIndex = false;
-
-    /**
-     * @var boolean metaFollow
-     *
-     * @MongoDB\Field(type="boolean")
-     */
-    protected $metaFollow = false;
 
     /**
      * @var array $languages
@@ -162,70 +137,6 @@ class Site implements SiteInterface
     public function getDefaultLanguage()
     {
         return $this->defaultLanguage;
-    }
-
-    /**
-     * @param string $metaKeywords
-     */
-    public function setMetaKeywords($metaKeywords)
-    {
-        $this->metaKeywords = $metaKeywords;
-    }
-
-    /**
-     * @return string
-     */
-    public function getMetaKeywords()
-    {
-        return $this->metaKeywords;
-    }
-
-    /**
-     * @param string $metaDescription
-     */
-    public function setMetaDescription($metaDescription)
-    {
-        $this->metaDescription = $metaDescription;
-    }
-
-    /**
-     * @return string
-     */
-    public function getMetaDescription()
-    {
-        return $this->metaDescription;
-    }
-
-    /**
-     * @param boolean $index
-     */
-    public function setMetaIndex($metaIndex)
-    {
-        $this->metaIndex = $metaIndex;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function getMetaIndex()
-    {
-        return $this->metaIndex;
-    }
-
-    /**
-     * @param boolean $follow
-     */
-    public function setMetaFollow($metaFollow)
-    {
-        $this->metaFollow = $metaFollow;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function getMetaFollow()
-    {
-        return $this->metaFollow;
     }
 
     /**
