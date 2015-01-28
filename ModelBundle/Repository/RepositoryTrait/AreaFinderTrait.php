f39267b7cfd9ee0fba5856f00e6781137b74ab9d
Nicolas Thal <nico.th4l@gmail.com>
2015-01-28 11:22:55 +0100

    add area finder

---
+++ ModelBundle/Repository/RepositoryTrait/AreaFinderTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PHPOrchestra\ModelBundle\Repository\RepositoryTrait;
+
+use PHPOrchestra\ModelInterface\Model\AreaContainerInterface;
+use PHPOrchestra\ModelInterface\Model\AreaInterface;
+
+/**
+ * Trait AreaFinderTrait
+ */
+trait AreaFinderTrait
+{
+    /**
+     * @param AreaContainerInterface $area
+     * @param string                 $areaId
+     *
+     * @return null|AreaInterface
+     */
+    public function findAreaByAreaId(AreaContainerInterface $area, $areaId)
+    {
+        foreach ($area->getAreas() as $subArea) {
+            if ($areaId == $subArea->getAreaId()) {
+                return $subArea;
+            }
+            if ($selectedArea = $this->findAreaByAreaId($subArea, $areaId)) {
+                return $selectedArea;
+            }
+        }
+
+        return null;
+    }
+}
