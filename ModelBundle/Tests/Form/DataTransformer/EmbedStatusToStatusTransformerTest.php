d2181016ba43bba71f819e84183fcd5116c361f9
Julien Chichignoud <julien.chichignoud@gmail.com>
2015-03-26 16:43:52 +0100

    update tests structure

---
+++ ModelBundle/Tests/Form/DataTransformer/EmbedStatusToStatusTransformerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace OpenOrchestra\ModelBundle\Tests\Form\DataTransformer;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Phake;
+use OpenOrchestra\ModelBundle\Form\DataTransformer\EmbedStatusToStatusTransformer;
+
+/**
+ * Class EmbedStatusToStatusTransformerTest
+ */
+class EmbedStatusToStatusTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EmbedStatusToStatusTransformer
+     */
+    protected $transformer;
+
+    protected $status;
+    protected $statusId;
+    protected $embedStatus;
+    protected $statusRepository;
+    protected $embedStatusClass;
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        $this->embedStatusClass = 'OpenOrchestra\ModelBundle\Document\EmbedStatus';
+        $this->statusId = 'statusId';
+
+        $this->status = Phake::mock('OpenOrchestra\ModelInterface\Model\StatusInterface');
+        Phake::when($this->status)->getId()->thenReturn($this->statusId);
+        Phake::when($this->status)->getToRoles()->thenReturn(new ArrayCollection());
+        Phake::when($this->status)->getFromRoles()->thenReturn(new ArrayCollection());
+
+        $this->embedStatus = Phake::mock('OpenOrchestra\ModelBundle\Document\EmbedStatus');
+        Phake::when($this->embedStatus)->getId()->thenReturn($this->statusId);
+
+        $this->statusRepository = Phake::mock('OpenOrchestra\ModelInterface\Repository\StatusRepositoryInterface');
+        Phake::when($this->statusRepository)->find(Phake::anyParameters())->thenReturn($this->status);
+
+        $this->transformer = new EmbedStatusToStatusTransformer($this->statusRepository, $this->embedStatusClass);
+    }
+
+    /**
+     * Test instance
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf('Symfony\Component\Form\DataTransformerInterface', $this->transformer);
+    }
+
+    /**
+     * Test transform
+     */
+    public function testTransform()
+    {
+        $status = $this->transformer->transform($this->embedStatus);
+
+        $this->assertSame($this->status, $status);
+    }
+
+    /**
+     * Test reverse transform
+     */
+    public function testReverseTransform()
+    {
+        $embedStatus = $this->transformer->reverseTransform($this->status);
+
+        $this->assertInstanceOf('OpenOrchestra\ModelInterface\Model\EmbedStatusInterface', $embedStatus);
+        $this->assertSame($this->statusId, $embedStatus->getId());
+    }
+
+    /**
+     * Test reverse transform
+     */
+    public function testReverseTransformWithNullElement()
+    {
+        $embedStatus = $this->transformer->reverseTransform(null);
+
+        $this->assertNull($embedStatus);
+    }
+}
---
+++ ModelBundle/Tests/Form/Type/OrchestraStatusTypeTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace OpenOrchestra\ModelBundle\Tests\Form\Type;
+
+use Phake;
+use OpenOrchestra\ModelBundle\Form\Type\OrchestraStatusType;
+
+/**
+ * Class OrchestraStatusTypeTest
+ */
+class OrchestraStatusTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var OrchestraStatusType
+     */
+    protected $form;
+
+    protected $builder;
+    protected $transformer;
+    protected $statusClass = 'statusClass';
+
+    /**
+     * Set up the text
+     */
+    public function setUp()
+    {
+        $this->builder = Phake::mock('Symfony\Component\Form\FormBuilder');
+        $this->transformer = Phake::mock('OpenOrchestra\ModelBundle\Form\DataTransformer\EmbedStatusToStatusTransformer');
+
+        $this->form = new OrchestraStatusType($this->transformer, $this->statusClass);
+    }
+
+    /**
+     * Test Name
+     */
+    public function testName()
+    {
+        $this->assertSame('orchestra_status', $this->form->getName());
+    }
+
+    /**
+     * Test Parent
+     */
+    public function testParent()
+    {
+        $this->assertSame('document', $this->form->getParent());
+    }
+
+    /**
+     * Test the default options
+     */
+    public function testSetDefaultOptions()
+    {
+        $resolverMock = Phake::mock('Symfony\Component\OptionsResolver\OptionsResolverInterface');
+
+        $this->form->setDefaultOptions($resolverMock);
+
+        Phake::verify($resolverMock)->setDefaults(array(
+            'embedded' => true,
+            'class' => $this->statusClass,
+            'property' => 'labels',
+        ));
+    }
+
+    /**
+     * @param array $config
+     * @param int   $times
+     *
+     * @dataProvider provideConfigAndCount
+     */
+    public function testBuildForm($config, $times)
+    {
+        $this->form->buildForm($this->builder, $config);
+
+        Phake::verify($this->builder, Phake::times($times))->addModelTransformer($this->transformer);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideConfigAndCount()
+    {
+        return array(
+            array(array('embedded' => true), 1),
+            array(array('embedded' => false), 0),
+        );
+    }
+}
