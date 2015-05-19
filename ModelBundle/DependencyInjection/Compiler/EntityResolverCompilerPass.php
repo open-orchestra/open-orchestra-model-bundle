ab9cb336f17f04a9d61b187a8165e98759d93a13
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-05-19 14:22:33 +0200

    add default resolve document in yml file

--- ModelBundle/DependencyInjection/Compiler/EntityResolverCompilerPass.php
+++ ModelBundle/DependencyInjection/Compiler/EntityResolverCompilerPass.php
@@ -4,6 +4,7 @@ namespace OpenOrchestra\ModelBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class EntityResolverCompilerPass
@@ -15,23 +16,7 @@ class EntityResolverCompilerPass implements  CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $defaultResolveDocument = array(
-            'OpenOrchestra\ModelInterface\Model\TranslatedValueInterface' => 'OpenOrchestra\ModelBundle\Document\TranslatedValue',
-            'OpenOrchestra\ModelInterface\Model\ReadSiteInterface' => 'OpenOrchestra\ModelBundle\Document\Site',
-            'FOS\UserBundle\Model\GroupInterface' => 'OpenOrchestra\GroupBundle\Document\Group',
-            'Symfony\Component\Security\Core\User\UserInterface' => 'OpenOrchestra\UserBundle\Document\User',
-            'OpenOrchestra\UserBundle\Model\ApiClientInterface' => 'OpenOrchestra\UserBundle\Document\ApiClient',
-            'OpenOrchestra\ModelInterface\Model\EmbedStatusInterface' => 'OpenOrchestra\ModelBundle\Document\EmbedStatus',
-            'OpenOrchestra\ModelInterface\Model\RoleInterface' => 'OpenOrchestra\ModelBundle\Document\Role',
-            'OpenOrchestra\ModelInterface\Model\AreaInterface' => 'OpenOrchestra\ModelBundle\Document\Area',
-            'OpenOrchestra\ModelInterface\Model\BlockInterface' => 'OpenOrchestra\ModelBundle\Document\Block',
-            'OpenOrchestra\ModelInterface\Model\StatusInterface' => 'OpenOrchestra\ModelBundle\Document\Status',
-            'OpenOrchestra\ModelInterface\Model\ThemeInterface' => 'OpenOrchestra\ModelBundle\Document\Theme',
-            'OpenOrchestra\ModelInterface\Model\SiteAliasInterface' => 'OpenOrchestra\ModelBundle\Document\SiteAlias',
-            'OpenOrchestra\ModelInterface\Model\ContentAttributeInterface' => 'OpenOrchestra\ModelBundle\Document\ContentAttribute',
-            'OpenOrchestra\ModelInterface\Model\FieldTypeInterface' => 'OpenOrchestra\ModelBundle\Document\FieldType',
-            'OpenOrchestra\ModelInterface\Model\FieldOptionInterface' => 'OpenOrchestra\ModelBundle\Document\FieldOption'
-        );
+        $defaultResolveDocument = Yaml::parse(file_get_contents(__DIR__.'/../../Resources/config/resolve_document.yml'))['resolve_target_documents'];
 
         $definition = $container->findDefinition('doctrine_mongodb.odm.listeners.resolve_target_document');
         $definitionCalls = $definition->getMethodCalls();
