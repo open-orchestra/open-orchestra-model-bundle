a948936cb6f3d21cba5387f8184b6d11604ec223
Nicolas Thal <nico.th4l@gmail.com>
2015-07-03 11:44:51 +0200

    fix tests

--- ModelBundle/Tests/Functional/Repository/NodeRepositoryTest.php
+++ ModelBundle/Tests/Functional/Repository/NodeRepositoryTest.php
@@ -211,7 +211,7 @@ class NodeRepositoryTest extends KernelTestCase
         $nodes = $this->repository->findLastVersionBySiteId($siteId);
 
         $this->assertCount($nodeNumber, $nodes);
-        $this->assertSameNode('fr', $version, $siteId, $nodes[NodeInterface::ROOT_NODE_ID]);
+        $this->assertSameNode('en', $version, $siteId, $nodes[NodeInterface::ROOT_NODE_ID]);
     }
 
     /**
@@ -388,7 +388,7 @@ class NodeRepositoryTest extends KernelTestCase
     public function provideLanguageSiteIdAndCount()
     {
         return array(
-            array('en', '2', 1),
+            array('en', '2', 2),
             array('fr', '2', 5),
         );
     }
