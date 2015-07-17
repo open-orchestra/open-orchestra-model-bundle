8a7bfefe2099e5f567607c400ebdf0aeb39c80f1
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-07-17 16:03:13 +0200

    add mongo serveur auto-increment for version on node

--- ModelBundle/DataFixtures/MongoDB/LoadNodeDemoData.php
+++ ModelBundle/DataFixtures/MongoDB/LoadNodeDemoData.php
@@ -26,6 +26,36 @@ class LoadNodeDemoData extends AbstractFixture implements OrderedFixtureInterfac
      */
     public function load(ObjectManager $manager)
     {
+
+        $manager->getConnection()->initialize();
+        $dataBase = $manager->getDocumentDatabase('OpenOrchestra\ModelBundle\Document\Node');
+        $dataBase->execute("
+db.node.createIndex( { nodeId: 1, siteId: 1, language: 1, version: 1 }, { unique: true } )
+db.system.js.save(
+    {
+        _id : 'duplicateNode' ,
+        value : function (data){
+            var node = null
+            while (1) {
+                var cursor = db.node.find( { nodeId: data.nodeId, siteId: data.siteId, language: data.language }).sort( { version: -1 } ).limit(1);
+                node = cursor.next()
+                delete node._id;
+                node.version = node.version + 1;
+                var results = db.node.insert(node);
+                if( results.hasWriteError() ) {
+                    if( results.getWriteError().code == 11000)
+                        continue;
+                    else
+                        print( 'unexpected error inserting data: ' + tojson( results ) );
+                }
+                break;
+            }
+            return node;
+        }
+    }
+);
+");
+
         $references = array();
         $references["status-published"] = $this->getReference('status-published');
         $references["status-draft"] = $this->getReference('status-draft');
