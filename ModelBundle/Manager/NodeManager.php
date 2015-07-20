fc8ea840e8dfb6ee058b05294e8e3f15d224e2ed
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-07-20 12:35:52 +0200

    delete dependency from mongo in controller

--- ModelBundle/Manager/NodeManager.php
+++ ModelBundle/Manager/NodeManager.php
@@ -4,22 +4,32 @@ namespace OpenOrchestra\ModelBundle\Manager;
 
 use OpenOrchestra\ModelBundle\Document\Area;
 use OpenOrchestra\ModelInterface\Model\NodeInterface;
+use OpenOrchestra\ModelInterface\Manager\NodeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
 /**
  * Class NodeManager
  */
-class NodeManager
+class NodeManager  extends ContainerAware implements NodeManagerInterface
 {
     protected $nodeClass;
 
     /**
-     * @param $nodeClass
+     * @param string     $nodeClass
      */
     public function __construct($nodeClass)
     {
         $this->nodeClass = $nodeClass;
     }
 
+    /**
+     * Create transverse node
+     *
+     * @param string $language
+     * @param string $siteId
+     *
+     * @return NodeInterface
+     */
     public function createTransverseNode($language, $siteId)
     {
         $area = new Area();
@@ -36,5 +46,27 @@ class NodeManager
         $node->addArea($area);
 
         return $node;
+    }
+
+    /**
+     * Duplicate a node
+     *
+     * @param string $nodeId
+     * @param string $siteId
+     * @param string $language
+     *
+     * @return NodeInterface
+     */
+    public function duplicateNode($nodeId, $siteId, $language)
+    {
+        $documentManager = $this->container->get('doctrine.odm.mongodb.document_manager');
+        $documentManager->getConnection()->initialize();
+        $dataBase = $documentManager->getDocumentDatabase($this->nodeClass);
+        $return = $dataBase->execute('db.loadServerScripts();return duplicateNode({ nodeId: \''.$nodeId.'\', siteId: \''.$siteId.'\', language: \''.$language.'\' });');
+
+        $newNode = new $this->nodeClass();
+        $documentManager->getHydratorFactory()->hydrate($newNode, $return['retval']);
+
+        return $newNode;
     }
 }
