e6305aef82e78a6072ad965a5a369875aa199a74
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-07-22 16:13:37 +0200

    add comment

--- ModelBundle/Migrations/MongoDB/Version20150722104732.php
+++ ModelBundle/Migrations/MongoDB/Version20150722104732.php
@@ -18,12 +18,18 @@ class Version20150722104732 extends AbstractMigration
         return "create stored procedure to increase node version";
     }
 
+    /**
+     * @param Database $db
+     */
     public function up(Database $db)
     {
         $this->executeScript($db, 'create_duplicate_node_20150722104732.js');
 
     }
 
+    /**
+     * @param Database $db
+     */
     public function down(Database $db)
     {
         $this->executeScript($db, 'delete_duplicate_node_20150722104732.js');
