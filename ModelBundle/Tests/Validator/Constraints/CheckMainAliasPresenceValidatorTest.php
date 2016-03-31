9cb460c0f7d3aa3753655655b5c8790963d524da
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-03-31 09:27:00 +0200

    add constraint on main alias

---
+++ ModelBundle/Tests/Validator/Constraints/CheckMainAliasPresenceValidatorTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace OpenOrchestra\ModelBundle\Tests\Validator\Constraints;
+
+use OpenOrchestra\BaseBundle\Tests\AbstractTest\AbstractBaseTestCase;
+use Phake;
+use OpenOrchestra\ModelBundle\Validator\Constraints\CheckMainAliasPresenceValidator;
+
+/**
+ * Class CheckMainAliasPresenceValidatorTest
+ */
+class CheckMainAliasPresenceValidatorTest extends AbstractBaseTestCase
+{
+    /**
+     * @var CheckMainAliasPresenceValidator
+     */
+    protected $validator;
+
+    protected $site;
+    protected $siteAlias;
+    protected $context;
+    protected $constraint;
+    protected $constraintViolationBuilder;
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        $this->constraint = Phake::mock('Symfony\Component\Validator\Constraint');
+        $this->context = Phake::mock('Symfony\Component\Validator\Context\ExecutionContextInterface');
+        $this->constraintViolationBuilder = Phake::mock('Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface');
+
+        Phake::when($this->context)->buildViolation(Phake::anyParameters())->thenReturn($this->constraintViolationBuilder);
+        Phake::when($this->constraintViolationBuilder)->atPath(Phake::anyParameters())->thenReturn($this->constraintViolationBuilder);
+
+        $this->siteAlias = Phake::mock('OpenOrchestra\ModelInterface\Model\SiteAliasInterface');
+        $this->site = Phake::mock('OpenOrchestra\ModelInterface\Model\SiteInterface');
+        Phake::when($this->site)->getMainAlias()->thenReturn($this->siteAlias);
+
+        $this->validator = new CheckMainAliasPresenceValidator();
+        $this->validator->initialize($this->context);
+    }
+
+    /**
+     * Test instance
+     */
+    public function testClass()
+    {
+        $this->assertInstanceOf('Symfony\Component\Validator\ConstraintValidator', $this->validator);
+    }
+
+    /**
+     * @param int $count
+     * @param int $violationTimes
+     *
+     * @dataProvider provideCountAndViolation
+     */
+    public function testAddViolationOrNot($isMain, $violationTimes)
+    {
+        Phake::when($this->siteAlias)->isMain()->thenReturn($isMain);
+
+        $this->validator->validate($this->node, $this->constraint);
+
+        Phake::verify($this->constraintViolationBuilder, Phake::times($violationTimes));
+    }
+
+    /**
+     * @return array
+     */
+    public function provideCountAndViolation()
+    {
+        return array(
+            array(true, 0),
+            array(false, 1),
+        );
+    }
+}
---
+++ ModelBundle/Validator/Constraints/CheckMainAliasPresence.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OpenOrchestra\ModelBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+class CheckMainAliasPresence extends Constraint
+{
+    public $message = 'open_orchestra_model_validators.document.website.exists_main_alias';
+
+    /**
+     * @return array|string
+     */
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
