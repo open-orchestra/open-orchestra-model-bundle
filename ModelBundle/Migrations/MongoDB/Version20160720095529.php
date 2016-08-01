a414b2f88d4c7a05b1c1a30f9bc99515e73470a0
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-08-01 12:01:49 +0200

    factorize migration

--- ModelBundle/Migrations/MongoDB/Version20160720095529.php
+++ ModelBundle/Migrations/MongoDB/Version20160720095529.php
@@ -2,13 +2,12 @@
 
 namespace OpenOrchestra\ModelBundle\Migrations\MongoDB;
 
-use AntiMattr\MongoDB\Migrations\AbstractMigration;
 use Doctrine\MongoDB\Database;
 
 /**
  * Class Version20160720095529
  */
-class Version20160720095529 extends AbstractMigration
+class Version20160720095529 extends AbstractAreaMigration
 {
     /**
      * @return string
@@ -23,25 +22,31 @@ class Version20160720095529 extends AbstractMigration
      */
     public function up(Database $db)
     {
-        $db->execute('
-            var updateArea = function(area) {
-                area.areaType = \'column\';
-                area.width = 1;
-                var subAreas = []
-                for (var i in area.subAreas) {
-                   var subArea = area.subAreas[i];
-                   subAreas.push(updateArea(subArea));
-                }
-                area.subAreas = subAreas;
+        dump($this->getUpdateAreaFunction().'
 
-                var rowArea = {}
-                rowArea.areaType = \'row\';
-                rowArea.areaId = \'row_\'+area.areaId;
-                rowArea.subAreas = [];
-                rowArea.subAreas.push(area);
+            db.template.find().forEach(function(item) {
+                 var rootArea = {}
+                 rootArea.label = \'Root\';
+                 rootArea.areaType = \'root\';
+                 rootArea.areaId = \'root\';
+                 rootArea.subAreas = [];
 
-                return rowArea;
-            }
+                 var areas = item.areas;
+                 for (var i in areas) {
+                    var subAreas = areas[i];
+                    rowArea = updateArea(subAreas);
+                    rootArea.subAreas.push(rowArea);
+                 }
+
+                 item.rootArea = rootArea;
+                 delete item.areas;
+                 db.template.update({ _id: item._id }, item);
+
+            });
+            ');
+        $db->execute(
+            $this->getUpdateAreaFunction().'
+
             db.template.find().forEach(function(item) {
                  var rootArea = {}
                  rootArea.label = \'Root\';
@@ -69,20 +74,8 @@ class Version20160720095529 extends AbstractMigration
      */
     public function down(Database $db)
     {
-        $db->execute('
-            var cleanRowAreas = function(rowAreas) {
-                var subareas = []
-                for (var i in rowAreas) {
-                    var rowArea = rowAreas[i];
-                    var columnArea = rowArea.subAreas[0];
-                    delete columnArea.areaType;
-                    delete columnArea.width;
-                    columnArea.subAreas = cleanRowAreas(columnArea.subAreas);
-                    subareas.push(columnArea)
-                }
-
-                return subareas;
-            }
+        $db->execute(
+            $this->getCleanRowAreasFunction() .'
 
             db.template.find().forEach(function(item) {
                  var rowAreas = item.rootArea.subAreas;
--- ModelBundle/Migrations/MongoDB/Version20160720134051.php
+++ ModelBundle/Migrations/MongoDB/Version20160720134051.php
@@ -8,7 +8,7 @@ use Doctrine\MongoDB\Database;
 /**
  * Class Version20160720134051
  */
-class Version20160720134051 extends AbstractMigration
+class Version20160720134051 extends AbstractAreaMigration
 {
     /**
      * @return string
@@ -23,25 +23,9 @@ class Version20160720134051 extends AbstractMigration
      */
     public function up(Database $db)
     {
-        $db->execute('
-            var updateArea = function(area) {
-                area.areaType = \'column\';
-                area.width = 1;
-                var subAreas = []
-                for (var i in area.subAreas) {
-                   var subArea = area.subAreas[i];
-                   subAreas.push(updateArea(subArea));
-                }
-                area.subAreas = subAreas;
+        $db->execute(
+            $this->getUpdateAreaFunction().'
 
-                var rowArea = {}
-                rowArea.areaType = \'row\';
-                rowArea.areaId = \'row_\'+area.areaId;
-                rowArea.subAreas = [];
-                rowArea.subAreas.push(area);
-
-                return rowArea;
-            }
             db.node.find().forEach(function(item) {
                  var rootArea = {}
                  rootArea.label = \'Root\';
@@ -70,20 +54,8 @@ class Version20160720134051 extends AbstractMigration
      */
     public function down(Database $db)
     {
-        $db->execute('
-            var cleanRowAreas = function(rowAreas) {
-                var subareas = []
-                for (var i in rowAreas) {
-                    var rowArea = rowAreas[i];
-                    var columnArea = rowArea.subAreas[0];
-                    delete columnArea.areaType;
-                    delete columnArea.width;
-                    columnArea.subAreas = cleanRowAreas(columnArea.subAreas);
-                    subareas.push(columnArea)
-                }
-
-                return subareas;
-            }
+        $db->execute(
+            $this->getCleanRowAreasFunction() .'
 
             db.node.find().forEach(function(item) {
                  var rowAreas = item.rootArea.subAreas;
