6c34d6042e21c79ac52b0b01c89c914afe4b3998
itkg-ngilain <noel.gilain@businessdecision.com>
2016-08-25 15:29:54 +0200

    Refacto UseTrackableTrait to be more genereic and no media dependant

--- ModelBundle/Repository/RepositoryTrait/UseTrackableTrait.php
+++ ModelBundle/Repository/RepositoryTrait/UseTrackableTrait.php
@@ -2,65 +2,22 @@
 
 namespace OpenOrchestra\ModelBundle\Repository\RepositoryTrait;
 
-use OpenOrchestra\ModelInterface\Model\UseTrackableInterface;
-
 /**
  * Trait UseTrackableTrait
  */
 trait UseTrackableTrait
 {
     /**
-     * @param string $nodeId
+     * @param string $entityId
+     * @param string $entityType
      *
      * @return array
      */
-    public function findUsedInNode($nodeId)
+    public function findByUsedInEntity($entityId, $entityType)
     {
         $qb = $this->createQueryBuilder();
 
-        $qb->field('useReferences.' . UseTrackableInterface::KEY_NODE . '.' . $nodeId)->exists('true');
-
-        return $qb->getQuery()->execute();
-    }
-
-    /**
-     * @param string $contentId
-     *
-     * @return array
-     */
-    public function findUsedInContent($contentId)
-    {
-        $qb = $this->createQueryBuilder();
-
-        $qb->field('useReferences.' . UseTrackableInterface::KEY_CONTENT . '.' . $contentId)->exists('true');
-
-        return $qb->getQuery()->execute();
-    }
-
-    /**
-     * @param string $contentId
-     *
-     * @return array
-     */
-    public function findUsedInContentType($contentId)
-    {
-        $qb = $this->createQueryBuilder();
-
-        $qb->field('useReferences.' . UseTrackableInterface::KEY_CONTENT_TYPE . '.' . $contentId)->exists('true');
-
-        return $qb->getQuery()->execute();
-    }
-
-    /**
-     * @param string $mediaId
-     *
-     * @return array
-     */
-    public function findUsedInMedia($mediaId)
-    {
-        $qb = $this->createQueryBuilder();
-
-        $qb->field('useReferences.' . UseTrackableInterface::KEY_MEDIA . '.' . $mediaId)->exists('true');
+        $qb->field('useReferences.' . $entityType . '.' . $entityId)->exists('true');
 
         return $qb->getQuery()->execute();
     }
