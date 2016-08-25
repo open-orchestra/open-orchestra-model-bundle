2b4d3c3b8e194056ae9ed4973c700a60e662d6a9
itkg-ngilain <noel.gilain@businessdecision.com>
2016-08-25 14:49:04 +0200

    Add keyword references

--- ModelBundle/Repository/KeywordRepository.php
+++ ModelBundle/Repository/KeywordRepository.php
@@ -6,6 +6,7 @@ use OpenOrchestra\ModelInterface\Model\KeywordInterface;
 use OpenOrchestra\ModelInterface\Repository\KeywordRepositoryInterface;
 use OpenOrchestra\Pagination\MongoTrait\PaginationTrait;
 use OpenOrchestra\Repository\AbstractAggregateRepository;
+use OpenOrchestra\ModelInterface\Model\UseTrackableInterface;
 
 /**
  * Class KeywordRepository
@@ -30,5 +31,61 @@ class KeywordRepository extends AbstractAggregateRepository implements KeywordRe
     public function getManager()
     {
         return $this->getDocumentManager();
+    }
+
+    /**
+     * @param string $nodeId
+     *
+     * @return array
+     */
+    public function findUsedInNode($nodeId)
+    {
+        $qb = $this->createQueryBuilder();
+
+        $qb->field('useReferences.' . UseTrackableInterface::KEY_NODE . '.' . $nodeId)->exists('true');
+
+        return $qb->getQuery()->execute();
+    }
+
+    /**
+     * @param string $contentId
+     *
+     * @return array
+     */
+    public function findUsedInContent($contentId)
+    {
+        $qb = $this->createQueryBuilder();
+
+        $qb->field('useReferences.' . UseTrackableInterface::KEY_CONTENT . '.' . $contentId)->exists('true');
+
+        return $qb->getQuery()->execute();
+    }
+
+    /**
+     * @param string $contentId
+     *
+     * @return array
+     */
+    public function findUsedInContentType($contentId)
+    {
+        $qb = $this->createQueryBuilder();
+    
+        $qb->field('useReferences.' . UseTrackableInterface::KEY_CONTENT_TYPE . '.' . $contentId)->exists('true');
+    
+        return $qb->getQuery()->execute();
+    }
+
+    /**
+     * @param string $mediaId
+     *
+     * @return array
+     */
+    public function findUsedInMedia($mediaId)
+    {
+        $qb = $this->createQueryBuilder();
+
+        $qb->field('useReferences.' . UseTrackableInterface::KEY_MEDIA . '.' . $mediaId)->exists('true');
+
+        return $qb->getQuery()->execute();
     }
 }
