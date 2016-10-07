eb4bc0143be20fbd861dc3faf97adf16c1f281fd
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-10-07 10:35:37 +0200

    fix content search, add filter current site (#662)
    * fix content search, add filter current site
    
    * remove useless filter

--- ModelBundle/Repository/ContentRepository.php
+++ ModelBundle/Repository/ContentRepository.php
@@ -101,14 +101,17 @@ class ContentRepository extends AbstractAggregateRepository implements FieldAuto
      * @param string      $contentType
      * @param string      $choiceType
      * @param string|null $condition
+     * @param string|null $siteId
      *
      * @return array
      */
-    public function findByContentTypeAndCondition($language, $contentType = '', $choiceType = self::CHOICE_AND, $condition = null)
+    public function findByContentTypeAndCondition($language, $contentType = '', $choiceType = self::CHOICE_AND, $condition = null, $siteId = null)
     {
         $qa = $this->createAggregationQuery();
         $qa->match($this->generateFilterPublishedNotDeletedOnLanguage($language));
-
+        if (!is_null($siteId)) {
+            $qa->match($this->generateSiteIdAndNotLinkedFilter($siteId));
+        }
         $filter = $this->generateContentTypeFilter($contentType);
 
         if ($filter && $condition) {
