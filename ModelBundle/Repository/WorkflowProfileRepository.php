a3ebf657147c8ddac7844aa190d9ad1c368ec5b4
itkg-ngilain <noel.gilain@businessdecision.com>
2016-12-06 17:27:06 +0100

    Add WorkflowProfileRepository::hasTransition

--- ModelBundle/Repository/WorkflowProfileRepository.php
+++ ModelBundle/Repository/WorkflowProfileRepository.php
@@ -4,10 +4,27 @@ namespace OpenOrchestra\ModelBundle\Repository;
 
 use OpenOrchestra\Repository\AbstractAggregateRepository;
 use OpenOrchestra\ModelInterface\Repository\WorkflowProfileRepositoryInterface;
+use OpenOrchestra\ModelInterface\Model\WorkflowProfileInterface;
+use OpenOrchestra\ModelInterface\Model\WorkflowTransitionInterface;
 
 /**
  * Class WorkflowProfileRepository
  */
 class WorkflowProfileRepository extends AbstractAggregateRepository implements WorkflowProfileRepositoryInterface
 {
+    /**
+     * Test is $transition exists
+     *
+     * @param  WorkflowTransitionInterface $transition
+     *
+     * @return boolean
+     */
+    public function hasTransition(WorkflowTransitionInterface $transition)
+    {
+        $qa = $this->createAggregationQuery();
+        $qa->match(array('transitions' => $transition));
+        $profile = $this->singleHydrateAggregateQuery($qa);
+
+        return $profile instanceof WorkflowProfileInterface;
+    }
 }
