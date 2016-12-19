83ccb5918c0a79025a6a69b234a696bd4a2cd6be
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-12-19 11:15:05 +0100

    paginate workflow

--- ModelBundle/Repository/WorkflowProfileRepository.php
+++ ModelBundle/Repository/WorkflowProfileRepository.php
@@ -2,10 +2,12 @@
 
 namespace OpenOrchestra\ModelBundle\Repository;
 
+use OpenOrchestra\Pagination\Configuration\PaginateFinderConfiguration;
 use OpenOrchestra\Repository\AbstractAggregateRepository;
 use OpenOrchestra\ModelInterface\Repository\WorkflowProfileRepositoryInterface;
 use OpenOrchestra\ModelInterface\Model\WorkflowProfileInterface;
 use OpenOrchestra\ModelInterface\Model\StatusInterface;
+use Solution\MongoAggregation\Pipeline\Stage;
 
 /**
  * Class WorkflowProfileRepository
@@ -45,5 +47,66 @@ class WorkflowProfileRepository extends AbstractAggregateRepository implements W
         $profile = $this->singleHydrateAggregateQuery($qa);
 
         return $profile instanceof WorkflowProfileInterface;
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     *
+     * @return array
+     */
+    public function findForPaginate(PaginateFinderConfiguration $configuration)
+    {
+        $qa = $this->createAggregationQuery();
+
+        $this->filterSearch($configuration, $qa);
+
+        $order = $configuration->getOrder();
+        if (!empty($order)) {
+            $qa->sort($order);
+        }
+
+        $qa->skip($configuration->getSkip());
+        $qa->limit($configuration->getLimit());
+
+        return $this->hydrateAggregateQuery($qa);
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     *
+     * @return int
+     */
+    public function countWithFilter(PaginateFinderConfiguration $configuration)
+    {
+        $qa = $this->createAggregationQuery();
+        $this->filterSearch($configuration, $qa);
+
+        return $this->countDocumentAggregateQuery($qa);
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        $qa = $this->createAggregationQuery();
+
+        return $this->countDocumentAggregateQuery($qa);
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     * @param Stage                       $qa
+     *
+     * @return array
+     */
+    protected function filterSearch(PaginateFinderConfiguration $configuration, Stage $qa)
+    {
+        $search = $configuration->getSearchIndex('label');
+        if (null !== $search && $search !== '') {
+            $qa->match(array('label' => new \MongoRegex('/.*'.$search.'.*/i')));
+        }
+
+        return $qa;
     }
 }
