c3a42a589a6a0c277038c552852462558578490f
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-01-02 11:29:33 +0100

    Add paginate workflow (#712)
    * paginate workflow
    
    * update workflow profile

--- ModelBundle/Repository/WorkflowProfileRepository.php
+++ ModelBundle/Repository/WorkflowProfileRepository.php
@@ -2,10 +2,12 @@
 
 namespace OpenOrchestra\ModelBundle\Repository;
 
+use OpenOrchestra\Pagination\Configuration\PaginateFinderConfiguration;
 use OpenOrchestra\Repository\AbstractAggregateRepository;
 use OpenOrchestra\ModelInterface\Repository\WorkflowProfileRepositoryInterface;
 use OpenOrchestra\ModelInterface\Model\WorkflowProfileInterface;
 use OpenOrchestra\ModelInterface\Model\StatusInterface;
+use Solution\MongoAggregation\Pipeline\Stage;
 
 /**
  * Class WorkflowProfileRepository
@@ -45,5 +47,84 @@ class WorkflowProfileRepository extends AbstractAggregateRepository implements W
         $profile = $this->singleHydrateAggregateQuery($qa);
 
         return $profile instanceof WorkflowProfileInterface;
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     *
+     * @return array
+     */
+    public function findForPaginate(PaginateFinderConfiguration $configuration)
+    {
+        $qa = $this->createAggregationQuery();
+
+        $this->filterSearch($configuration, $qa);
+
+        $order = $configuration->getOrder();
+        if (!empty($order)) {
+            $qa->sort($order);
+        }
+
+        $qa->skip($configuration->getSkip());
+        $qa->limit($configuration->getLimit());
+
+        return $this->hydrateAggregateQuery($qa);
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     *
+     * @return int
+     */
+    public function countWithFilter(PaginateFinderConfiguration $configuration)
+    {
+        $qa = $this->createAggregationQuery();
+        $this->filterSearch($configuration, $qa);
+
+        return $this->countDocumentAggregateQuery($qa);
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        $qa = $this->createAggregationQuery();
+
+        return $this->countDocumentAggregateQuery($qa);
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     * @param Stage                       $qa
+     *
+     * @return array
+     */
+    protected function filterSearch(PaginateFinderConfiguration $configuration, Stage $qa)
+    {
+        $label = $configuration->getSearchIndex('label');
+        $language = $configuration->getSearchIndex('language');
+
+        if (null !== $label && '' !== $label && null !== $language && '' !== $language) {
+            $qa->match(array('labels.' . $language => new \MongoRegex('/.*'.$label.'.*/i')));
+        }
+
+        return $qa;
+    }
+
+    /**
+     * @param array $workflowProfileIds
+     *
+     * @throws \Doctrine\ODM\MongoDB\MongoDBException
+     */
+    public function removeWorkflowProfiles(array $workflowProfileIds)
+    {
+        array_walk($workflowProfileIds, function(&$workflowProfileId) {$workflowProfileId = new \MongoId($workflowProfileId);});
+
+        $qb = $this->createQueryBuilder();
+        $qb->remove()
+            ->field('id')->in($workflowProfileIds)
+            ->getQuery()
+            ->execute();
     }
 }
