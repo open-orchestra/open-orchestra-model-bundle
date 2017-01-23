7f2c4582379178e2ee3bd55e65e2139444d493c3
itkg-ngilain <noel.gilain@businessdecision.com>
2017-01-23 12:16:29 +0100

    Update RedirectionRepository filter methods

--- ModelBundle/Repository/RedirectionRepository.php
+++ ModelBundle/Repository/RedirectionRepository.php
@@ -3,16 +3,15 @@
 namespace OpenOrchestra\ModelBundle\Repository;
 
 use OpenOrchestra\ModelInterface\Repository\RedirectionRepositoryInterface;
-use OpenOrchestra\Pagination\MongoTrait\PaginationTrait;
 use OpenOrchestra\Repository\AbstractAggregateRepository;
+use OpenOrchestra\Pagination\Configuration\PaginateFinderConfiguration;
+use Solution\MongoAggregation\Pipeline\Stage;
 
 /**
  * Class RedirectionRepository
  */
 class RedirectionRepository extends AbstractAggregateRepository implements RedirectionRepositoryInterface
 {
-    use PaginationTrait;
-
     /**
      * @param string $nodeId
      * @param string $locale
@@ -45,5 +44,96 @@ class RedirectionRepository extends AbstractAggregateRepository implements Redir
         ));
 
         return $this->hydrateAggregateQuery($qa);
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     *
+     * @return array
+     */
+    public function findForPaginate(PaginateFinderConfiguration $configuration)
+    {
+        $qa = $this->createAggregationQuery();
+
+        $this->filterSearch($configuration, $qa);
+
+        $order = $configuration->getOrder();
+        if (!empty($order)) {
+            $qa->sort($order);
+        }
+
+        $qa->skip($configuration->getSkip());
+        $qa->limit($configuration->getLimit());
+
+        return $this->hydrateAggregateQuery($qa);
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        $qa = $this->createAggregationQuery();
+
+        return $this->countDocumentAggregateQuery($qa);
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     *
+     * @return int
+     */
+    public function countWithFilter(PaginateFinderConfiguration $configuration)
+    {
+        $qa = $this->createAggregationQuery();
+        $this->filterSearch($configuration, $qa);
+
+        return $this->countDocumentAggregateQuery($qa);
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     * @param Stage                       $qa
+     *
+     * @return array
+     */
+    protected function filterSearch(PaginateFinderConfiguration $configuration, Stage $qa)
+    {
+//         $qa->match(array('extra.site_id' => $configuration->getSearchIndex('site_id')));
+
+        $siteName = $configuration->getSearchIndex('site_name');
+        if (null !== $siteName && '' !== $siteName) {
+            $qa->match(array('siteName' => new \MongoRegex('/.*' . $siteName . '.*/i')));
+        }
+
+        $locale = $configuration->getSearchIndex('locale');
+        if (null !== $locale && '' !== $locale) {
+            $qa->match(array('locale' => new \MongoRegex('/.*' . $locale . '.*/i')));
+        }
+
+        $routePattern = $configuration->getSearchIndex('route_pattern');
+        if (null !== $routePattern && '' !== $routePattern) {
+            $qa->match(array('routePattern' => new \MongoRegex('/.*' . $routePattern . '.*/i')));
+        }
+
+        $redirection = $configuration->getSearchIndex('redirection');
+        if (null !== $redirection && '' !== $redirection) {
+            $qa->match(array('$or' => array(
+                array('url'    => new \MongoRegex('/.*' . $redirection . '.*/i')),
+                array('nodeId' => new \MongoRegex('/.*' . $redirection . '.*/i'))
+            )));
+        }
+
+        $permanent = $configuration->getSearchIndex('permanent');
+        if (null !== $redirection && '' !== $permanent) {
+            if ('true' == $permanent) {
+                $qa->match(array('permanent' => true));
+            }
+            if ('false' == $permanent) {
+                $qa->match(array('permanent' => false));
+            }
+        }
+
+        return $qa;
     }
 }
