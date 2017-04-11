0173066af336831032f728d0039d1247c5135eb7
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-04-11 14:59:56 +0200

    fix remove content out of worklow (#763)

--- ModelBundle/DataFixtures/MongoDB/LoadStatusData.php
+++ ModelBundle/DataFixtures/MongoDB/LoadStatusData.php
@@ -16,7 +16,7 @@ class LoadStatusData extends AbstractLoadStatus implements OrchestraProductionFi
      */
     public function load(ObjectManager $manager)
     {
-        $manager->persist($this->loadStatus('Out of validation workflow', 'Non soumis au workflow de validation', 'outOfWorkflow', 'grayDark', true, false, false, true));
+        $manager->persist($this->loadStatus('Out of validation workflow', 'Non soumis au workflow de validation', 'outOfWorkflow', 'grayDark', true, false, false, true, false, true));
         $manager->persist($this->loadStatus('Draft', 'Brouillon', 'draft', 'green', false, true));
         $manager->persist($this->loadStatus('Published', 'Publié', 'published', 'red', true, false, true));
         $manager->persist($this->loadStatus('To translate', 'A traduire', 'toTranslate', 'blue', false, false, false, false, false, false, true));
