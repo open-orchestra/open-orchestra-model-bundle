f92b9502cc335e8568276f360ee7af7bc629c8f2
itkg-nanne <nicolas.anne@businessdecision.com>
2017-06-07 12:24:14 +0200

    add reference command

---
+++ ModelBundle/Command/OrchestraGenerateReferenceCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace OpenOrchestra\ModelBundle\Command;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use OpenOrchestra\Backoffice\Reference\ReferenceManager;
+use OpenOrchestra\ModelBundle\Document\Block;
+use OpenOrchestra\ModelBundle\Document\Content;
+use OpenOrchestra\ModelBundle\Document\Node;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class OrchestraGenerateReferenceCommand
+ */
+class OrchestraGenerateReferenceCommand extends ContainerAwareCommand
+{
+    /**
+     * Configure command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('orchestra:references:generate')
+            ->setDescription('generate references');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return mixed
+     * @throws \InvalidArgumentException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dm = $this->getContainer()->get('doctrine.odm.mongodb.document_manager');
+        $referenceManager = $this->getContainer()->get('open_orchestra_backoffice.reference.manager');
+
+        $output->writeln(' + Update use references of nodes');
+        $this->updateUseReferenceEntity(Node::class, $dm, $referenceManager, $output);
+
+        $output->writeln(' + Update use references of blocks');
+        $this->updateUseReferenceEntity(Block::class, $dm, $referenceManager, $output);
+
+        $output->writeln(' + Update use references of contents');
+        $this->updateUseReferenceEntity(Content::class, $dm, $referenceManager, $output);
+    }
+
+    /**
+     * @param String           $entityClass
+     * @param DocumentManager  $dm
+     * @param ReferenceManager $referenceManager
+     * @param OutputInterface $output
+     */
+    protected function updateUseReferenceEntity($entityClass, DocumentManager $dm, ReferenceManager $referenceManager, OutputInterface $output)
+    {
+        $limit = 20;
+        $countEntities = $dm->createQueryBuilder($entityClass)->getQuery()->count();
+        for ($skip = 0; $skip < $countEntities; $skip += $limit) {
+            $output->writeln('  - Update references from '.$skip.' to '.($skip+$limit));
+            $entities = $dm->createQueryBuilder($entityClass)
+                ->skip($skip)
+                ->limit($limit)
+                ->getQuery()->execute();
+            foreach ($entities as $entity) {
+                $referenceManager->updateReferencesToEntity($entity);
+            }
+            $dm->clear();
+        }
+    }
+}
